301e8034220e2df6a6bf85561272c94ba945e7b7
Sathwik <sathwikreddy0303@gmail.com>
2025-12-12 21:37:11 +0530

    chore: prepare for render deployment

---
+++ test-mongo.js
@@ -0,0 +1,15 @@
+﻿const { MongoClient } = require('mongodb');
+const fs = require('fs');
+const uri = process.env.MONGODB_URI || (fs.existsSync('.env') && fs.readFileSync('.env','utf8').match(/MONGODB_URI=(.*)/)?.[1]);
+console.log('TEST: using URI ->', (uri && uri.length>80 ? uri.slice(0,80)+'...' : uri));
+(async () => {
+  try {
+    const client = new MongoClient(uri, { serverSelectionTimeoutMS: 8000 });
+    await client.connect();
+    console.log('TEST: connected OK');
+    await client.close();
+  } catch (err) {
+    console.error('TEST ERROR:', err && err.stack ? err.stack : err);
+    process.exitCode = 1;
+  }
+})();
